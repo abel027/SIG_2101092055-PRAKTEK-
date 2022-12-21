85e2936cced4fce84fdbe71b555afec568d774d2
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2022-12-21 13:20:58 +0700

    Create db.php

---
+++ pemetaan/db.php
@@ -0,0 +1,41 @@
+<?php
+class connectToDB {
+
+private $conn;
+
+public function __construct()
+
+{
+  $config = include 'konek.php';
+  $this->conn = new mysqli( $config['db']['server'], $config['db']['user'], $config['db']['pass'], $config['db']['dbname']);
+}
+
+function __destruct()
+{
+  $this->conn->close();
+}
+
+
+public function addCompany( $company, $details, $latitude, $longitude, $telephone){
+
+$statement = $this->conn->prepare("Insert INTO companies( company, details, latitude, longitude, telephone) VALUES(?, ?, ?, ?, ?)");
+
+$statement->bind_param( 'sssss',$company, $details, $latitude, $longitude, $telephone);
+
+$statement->execute();
+
+$statement->close();
+}
+
+public function getcompaniesList()
+{
+  $query ="select * from companies";
+  $data = mysqli_query($this->conn, $query);
+  while ($row = mysqli_fetch_array($data)){
+    $hasil[] = $row;
+  }
+    return $hasil;
+}
+
+}
+?>
